0ff1500afb3ffeb0b21db9af0bdbc44630e73706
jesse berkowitz <jberkow@brandeis.edu>
2023-01-21 18:57:56 -0800

    updated obstacles

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 from sys import exit
 import copy
 from player import  *
+import obstacle
 
 pygame.init()
 # Globals
@@ -9,6 +10,7 @@ WIDTH=800
 HEIGHT=800
 Health = 100
 BG = (30,30,30)
+Block_Color = (241,79,80)
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Space_Invaders')
@@ -17,10 +19,29 @@ class Game:
     def __init__(self):
         player_sprite = Player((WIDTH/2,HEIGHT),WIDTH,5)
         self.player = pygame.sprite.GroupSingle(player_sprite)
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        
+        self.create_multiple_obstacles(WIDTH*.15,WIDTH*.8,0,WIDTH*.33, WIDTH*.66)
+    def create_obstacle(self,x_start,y_start,offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index, val in enumerate(row):
+                if val =='x':
+                    x = col_index *self.block_size + x_start + offset_x
+                    y = row_index *self.block_size + y_start
+                    block = obstacle.Block(self.block_size,Block_Color,x,y)
+                    self.blocks.add(block) 
+    def create_multiple_obstacles(self, x_start,y_start, *offset):
+        for offset_x in offset:
+            self.create_obstacle(x_start,y_start,offset_x)
+
+
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
+        self.blocks.draw(screen)
         # update and draw sprite groups
         
 
