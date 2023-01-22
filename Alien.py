cec51a2fff08cb5b18a77f7c6e901c0c83958aa6
jesse berkowitz <jberkow@brandeis.edu>
2023-01-22 11:48:36 -0800

    visuals working

--- Alien.py
+++ Alien.py
@@ -2,9 +2,22 @@ import pygame
 class Alien(pygame.sprite.Sprite):
     def __init__(self,color,x,y,WIDTH):
         super().__init__()
-        self.image = pygame.image.load(f'{color}.png')
+        self.image = pygame.image.load(f'{color}.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=(x,y))
         self.WIDTH=WIDTH                
     def update(self,speed):
         self.rect.x += speed
-       
+class Extra(pygame.sprite.Sprite):
+    def __init__(self,side,screen_width):
+        super().__init__()
+        self.image = pygame.image.load('extra.png').convert_alpha()
+        if side == 'right':
+            x = screen_width + 50
+            self.speed = -3
+        else:
+            x = -50
+            self.speed = 3   
+        self.rect = self.image.get_rect(topleft=(x,80))
+    def update(self):
+        self.rect.x +=self.speed
+
--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import pygame
 from sys import exit
-import random
-from random import choice
+
+from random import choice,randint
 from player import  *
 import obstacle
-from Alien import Alien
+from Alien import Alien, Extra
 from laser import Laser
 
 pygame.init()
@@ -25,23 +25,28 @@ class Game:
         self.shape = obstacle.shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.obstacle_amount = 8
+        self.obstacle_amount = 4
         self.obstacles = [x*(WIDTH/self.obstacle_amount) for x in range(self.obstacle_amount)]
         # calculate evening spacing buffer
         self.range =self.obstacles[-1]-self.obstacles[0]
         self.buffer = (WIDTH - self.range) / 2
         self.create_multiple_obstacles(self.buffer-30,WIDTH*.8,*self.obstacles)
+        # Alien setup
         self.alien_rows = 6
         self.alien_cols = 12
         self.alien_size = 40
         self.x_space = 10
         self.y_space = 10
+        self.y_buffer = 100
         self.x_alien_buffer = self.calc_buffer()
         self.alien_speed = 1
         self.alien_Lasers = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
         self.alien_setup(self.alien_rows,self.alien_cols)
-    
+        # Extra ship setup
+        self.extra = pygame.sprite.GroupSingle()
+        self.extra_timer = randint(40,80)
+
     def calc_buffer(self):
         return (WIDTH - ((self.alien_size+self.x_space)*self.alien_cols))/2
 
@@ -50,7 +55,7 @@ class Game:
         for row_idx,row in enumerate(range(rows)):
             for col_idx,col in enumerate(range(cols)):
                 x = col_idx * self.alien_size + self.x_alien_buffer + col* self.x_space
-                y = row_idx * self.alien_size + row*self.y_space
+                y = row_idx * self.alien_size + row*self.y_space + self.y_buffer
                 alien_sprite = Alien(colors[row_idx%len(colors)],x,y,\
                     WIDTH)
                 self.aliens.add(alien_sprite)
@@ -65,12 +70,19 @@ class Game:
                 return
 
     def alien_shoot(self):
-        if random.randint(0,100)>97:
-
+        if randint(0,100)>98:
             if self.aliens.sprites():
                 rand = choice(self.aliens.sprites())
                 laser = Laser(rand.rect.center,-3,HEIGHT)
                 self.alien_Lasers.add(laser)
+
+    def extra_alien_timer(self):
+        self.extra_timer -=1
+        if self.extra_timer<=0:
+            side = ['right', 'left']
+            self.extra.add(Extra(choice(side),WIDTH))
+            self.extra_timer = randint(400,800)
+
 
     def create_obstacle(self,x_start,y_start,offset_x):
         for row_index, row in enumerate(self.shape):
@@ -85,17 +97,52 @@ class Game:
         for offset_x in offset:
             self.create_obstacle(x_start,y_start,offset_x)
 
+    def collision_checks(self):
+        if self.player.sprite.lasers:
+            for laser in self.player.sprite.lasers:
+                if pygame.sprite.spritecollide(laser,self.blocks,True):
+                    laser.kill()
+                if pygame.sprite.spritecollide(laser,self.aliens,True):
+                    laser.kill()
+                if pygame.sprite.spritecollide(laser,self.extra,True):
+                    laser.kill()   
+
+            # for extra in self.extra:    
+        if self.alien_Lasers:
+            for laser in self.alien_Lasers:
+                if pygame.sprite.spritecollide(laser,self.blocks,True):
+                    laser.kill()
+                if pygame.sprite.spritecollide(laser,self.player,True):
+                    laser.kill()
+                    print('game over')
+                    exit()
+        if self.aliens:
+            for alien in self.aliens:
+                pygame.sprite.spritecollide(alien,self.blocks,True)                    
+                if pygame.sprite.spritecollide(alien,self.player,True):                    
+                    print('game over')
+                    exit()
+
+
+
+
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
         self.blocks.draw(screen)
+        
         self.aliens.update(self.alien_speed)
         self.change_dir(16)        
         self.aliens.draw(screen)
         self.alien_shoot()
         self.alien_Lasers.update() 
-        self.alien_Lasers.draw(screen)      
+        self.extra.update()
+
+        self.extra_alien_timer()
+        self.alien_Lasers.draw(screen)
+        self.extra.draw(screen)
+        self.collision_checks()      
         
 
 G = Game()
